19ec3addc0fac0697f7a4f5a98eb11857d70d674
franco <francoriquelme76@gmail.com>
2020-07-29 23:20:35 -0300

    se agregaron 3 funciones

---
+++ ejercicio.py
@@ -0,0 +1,15 @@
+class Producto:
+    def __init__(self, nombre, precio, primera_necesidad):
+        self.__nombre = nombre
+        self.__precio = precio
+        self.__primera_necesidad = primera_necesidad
+
+def get_nombre(self):
+    return self.__nombre
+def get primera_necesidad(self):
+    return self.__primera_necesidad
+def precio(self):
+    if primera_necesidad():
+        return self.__precio*0,9
+    else:
+        return self.__precio
